a24fb897417b122964fabdf8399ede0363b3d8d1
MagiMarinova <magi.marinowax@gmail.com>
2023-06-17 12:13:00 +0300

    Routes

--- app.js
+++ app.js
@@ -39,6 +39,58 @@ app.use(session({
     }
 }));
 
+
+app.route('/login')
+    .post(function(req, res){
+        console.log(req.cookies);
+        var loginQuery = 'select * from users where username = \'' + req.body.username +
+            '\' and password = \'' + req.body.password + '\';';
+        con.query(loginQuery, function (error, result) {
+            console.log(JSON.stringify(result));
+            if (JSON.stringify(result) == "[]") {
+                res.redirect('/');
+            } else {
+                var cookie = req.cookies.user;
+                if (cookie === undefined) {
+                    var randomNumber=Math.random().toString();
+                    randomNumber=randomNumber.substring(2,randomNumber.length);
+                    res.cookie('user',randomNumber, { maxAge: 4200000, httpOnly: true });
+                }
+                res.redirect('/stream');
+            }
+        });
+    });
+
+app.route('/androidLogin')
+    .post(function(req, res){
+        var loginQuery = 'select * from users where username = \'' + req.body.username +
+            '\' and password = \'' + req.body.password + '\';';
+        con.query(loginQuery, function (error, result) {
+            if (error) throw error;
+            console.log(result);
+            if (JSON.stringify(result) != "[]") {
+                res.send({
+                    isSuccessful: true
+                });
+            } else {
+                res.send({
+                    isSuccessful: false
+                });
+            }
+        });
+    });
+
+app.get('/stream', (req,res) => {
+    if(req.cookies.user)
+        res.render('stream');
+    else
+        res.redirect('/');
+});
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
 app.use(function(req, res, next) {
     next(createError(404));
 });
